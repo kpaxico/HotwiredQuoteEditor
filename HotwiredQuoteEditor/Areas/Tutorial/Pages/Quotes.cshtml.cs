bc3f1c83cea2b576bc032430db50b9ebcd21b270
kpaxico <ipekcyilmaz@gmail.com>
2022-05-14 16:37:57 +0300

    get ready for Turbo
    feat: add Areas\Tutorial
    feat: add Quotes.cshtml
    chore: move _ViewStart.cshtml to the project root

---
+++ HotwiredQuoteEditor/Areas/Tutorial/Pages/Quotes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HotwiredQuoteEditor.Areas.Tutorial.Pages
+{
+    public class QuotesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
